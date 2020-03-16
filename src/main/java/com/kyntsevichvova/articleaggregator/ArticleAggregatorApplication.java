ccb51479e1ae294be7889fabd617fc52fdd75b1c
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2020-03-16 23:55:51 +0300

    Initial commit

---
+++ src/main/java/com/kyntsevichvova/articleaggregator/ArticleAggregatorApplication.java
@@ -0,0 +1,11 @@
+package com.kyntsevichvova.articleaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleAggregatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleAggregatorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kyntsevichvova/articleaggregator/ArticleAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyntsevichvova.articleaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
